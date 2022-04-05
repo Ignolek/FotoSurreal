ca90d9a29b82e7b106219525b22933f0b71796d6
Ignolek <ignole@icloud.com>
2022-04-05 18:30:50 +0200

    Ortho cam works Perspective and orthogonal image is being render with one run

--- IK_Raytracer/IK_Raytracer/Camera.h
+++ IK_Raytracer/IK_Raytracer/Camera.h
@@ -2,6 +2,8 @@
 #include "Ray.h"
 class Camera
 {
+private:
+	vec3 _u, _v, _w;
 public:
 	vec3 lowerLeftCorner, horizontal, vertical, origin;
 	Camera()
@@ -16,25 +18,27 @@ public:
 	
 	Camera(vec3 lookFrom, vec3 lookAt, vec3 vUp, float vFov, float aspect)
 	{
-		vec3 u, v, w;
 		float theta = vFov * 3.1415 / 180;
 		float halfHeight = tan(theta / 2);
 		float halfWidth = aspect * halfHeight;
 		origin = lookFrom;
-		w = unit_vector(lookFrom - lookAt);
-		u = unit_vector(cross(vUp, w));
-		v = cross(w, u);
+		_w = unit_vector(lookFrom - lookAt);
+		_u = unit_vector(cross(vUp, _w));
+		_v = cross(_w, _u);
 		lowerLeftCorner = vec3(-halfWidth, -halfHeight, -1.0f);
-		lowerLeftCorner = origin - halfWidth * u - halfHeight * v - w;
-		horizontal = 2 * halfWidth * u;
-		vertical = 2 * halfHeight * v;
+		lowerLeftCorner = origin - halfWidth * _u - halfHeight * _v - _w;
+		horizontal = 2 * halfWidth * _u;
+		vertical = 2 * halfHeight * _v;
 	}
 	~Camera(){}
 	
-	Ray getRay(float u, float v, bool ortho)
+	Ray getRay(float u, float v, float fov, float m, bool ortho)
 	{
+		fov /= 100;
+		float multiplier = pow(2.42, m - 1);
 		if (ortho)
-			return Ray(vec3(horizontal.e[0] * u + 0.825, vertical.e[1] * v + 0.45, 0), vec3(horizontal.e[0] * u + 0.825, vertical.e[1] * v + 0.45, -1000));
+			//return Ray(vec3(horizontal.e[0] * u, vertical.e[1] * v, 0), vec3(horizontal.e[0] * u, vertical.e[1] * v , -10000));
+			return Ray(vec3(horizontal.e[0] * u + 0.825 * multiplier, vertical.e[1] * v + fov, 0), vec3(horizontal.e[0] * u + 0.825 * multiplier, vertical.e[1] * v + fov, -1000));
 			//return Ray(vec3(horizontal.e[0] * u - 2, vertical[1] * v - 1, 0), vec3(horizontal.e[0] * u - 2, vertical[1] * v - 1, -1000));
 		else
 			return Ray(origin, lowerLeftCorner + u * horizontal + v * vertical - origin);
--- IK_Raytracer/IK_Raytracer/IK_Raytracer.cpp
+++ IK_Raytracer/IK_Raytracer/IK_Raytracer.cpp
@@ -30,61 +30,87 @@ int main()
     int scrHeight = 1000;
     int depth = 1;
 
-    Image* blank = new Image(scrWidth, scrHeight, 3);
+    Image* orthogonal = new Image(scrWidth, scrHeight, 3);
+    Image* perspective = new Image(scrWidth, scrHeight, 3);
 
     LightIntensity lColor(220, 0, 0);
     Sphere sphere(vec3(0, 0, -1), 0.5);
     Hitable* list[3];
-    list[0] = new Sphere(vec3(0.5, 0.0, -1), 0.25);
+    list[0] = new Sphere(vec3(0.0, 0.0, -1), 0.25);
     list[1] = new Sphere(vec3(0, -100.5, -1), 100);
-    list[2] = new Sphere(vec3(-0.5, 0, -4), 0.25);
+    list[2] = new Sphere(vec3(-0.25, 0, -4), 0.25);
     Hitable* world = new HitableList(list, 3);
-    Camera cam(vec3(0, 0, 0), vec3(0.0, 0, -1), vec3(0, -1, 0), 45, float(scrWidth)/float(scrHeight));
-    bool ortho = false;
-    std::cout << "Do You want to use orthogonal camera? [0 -- no, 1 -- yes]: ";
-    std::cin >> ortho;
+    float fov = 45;
+    Camera cam(vec3(0, 0, 0), vec3(0, 0, -1), vec3(0, -1, 0), fov, float(scrWidth)/float(scrHeight));
+    bool ortho = true;
 
     float pixelWidth = 2 / scrWidth;
     float pixelHeight = 2 / scrHeight;
-
-    for (int i = 0; i < scrWidth; i++)
+    for (int imageNumber = 0; imageNumber < 2; imageNumber++)
     {
-        for (int j = 0; j < scrHeight; j++)
+        if (ortho == true)
+            std::cout << "Orthogonal image is rendering now..." << std::endl;
+        else
+            std::cout << "Perspective image is rendering now..." << std::endl;
+        for (int i = 0; i < scrWidth; i++)
         {
-            vec3 col(0, 0, 0);
-            for (int s = 0; s < depth; s++)
+            for (int j = 0; j < scrHeight; j++)
             {
-                float u = float(i) / float(scrWidth);
-                float v = float(j) / float(scrHeight);
+                vec3 col(0, 0, 0);
+                Ray r;
+                for (int s = 0; s < depth; s++)
+                {
+                    float u = float(i) / float(scrWidth);
+                    float v = float(j) / float(scrHeight);
 
-                float uMin = (-1.f + i * pixelWidth) * (90*(float(scrWidth) / float(scrHeight)));
-                float vMin = (1.f - (j + 1.0f) * pixelHeight) * 90;
+                    float uMin = (-1.f + i * pixelWidth) * (90 * (float(scrWidth) / float(scrHeight)));
+                    float vMin = (1.f - (j + 1.0f) * pixelHeight) * 90;
 
-                float uMax = (-1.f + (i + 1.0f) * pixelWidth) * (90 * (float(scrWidth) / float(scrHeight)));
-                float vMax = (1.f - j * pixelHeight) * 90;
+                    float uMax = (-1.f + (i + 1.0f) * pixelWidth) * (90 * (float(scrWidth) / float(scrHeight)));
+                    float vMax = (1.f - j * pixelHeight) * 90;
 
-                //LightIntensity color = GetColorByAntialiasing(world, uMin, uMax, vMin, vMax, i, j, 1.0f, vector<LightIntensity*>{nullptr, nullptr, nullptr, nullptr, nullptr});
+                    //LightIntensity color = GetColorByAntialiasing(world, uMin, uMax, vMin, vMax, i, j, 1.0f, vector<LightIntensity*>{nullptr, nullptr, nullptr, nullptr, nullptr});
 
-                float pixelCenterX = u + pixelWidth;
-                float pixelCenterY = v + pixelHeight;
+                    float pixelCenterX = u + pixelWidth;
+                    float pixelCenterY = v + pixelHeight;
 
-                Ray r = cam.getRay(u, v, ortho);
-                
-                //vec3 unit_direction = unit_vector()
-                col = color(r, world);
+                    if (ortho == true)
+                    {
+                        r = cam.getRay(u, v, fov, fov / 45, ortho);
+                        col = color(r, world);
+        
+                        int iR = int(255.99 * col[0]);
+                        int iG = int(255.99 * col[1]);
+                        int iB = int(255.99 * col[2]);
+
+                        lColor(iR, iG, iB);
+                        orthogonal->SetPixel(i, j, lColor);
+                    }
+
+                    else
+                    {
+                        r = cam.getRay(u, v, fov, fov / 45, ortho);
+                        col = color(r, world);
+
+                        int iR = int(255.99 * col[0]);
+                        int iG = int(255.99 * col[1]);
+                        int iB = int(255.99 * col[2]);
+
+                        lColor(iR, iG, iB);
+                        perspective->SetPixel(i, j, lColor);
+                    }
+                }
             }
-           
-            //col /= float(depth);
-
-            int iR = int(255.99 * col[0]);
-            int iG = int(255.99 * col[1]);
-            int iB = int(255.99 * col[2]);
-
-            lColor(iR, iG, iB);
-            blank->SetPixel(i, j, lColor);
         }
+        orthogonal->Write("ortho.png");
+        if(ortho == true)
+            std::cout << "Orthogonal image created!" << std::endl;
+
+        ortho = false;
     }
-    blank->Write("obrazek.bmp");
+
+    perspective->Write("perspective.png");
+    std::cout << "Pespective image created!" << std::endl;
 
     return 0;
 }
